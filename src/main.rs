1ca716468302097d67f9b189a6a73e214489b666
nordluma <maxnordlund.95@hotmail.com>
2024-06-11 23:59:06 +0300

    feat: setup a virtual interface in tun mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use tun_tap::Iface;
+
+fn main() -> io::Result<()> {
+    let nic = Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0_u8; 1504];
+    let n_bytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes: {:x?}", n_bytes, &buf[..n_bytes]);
+
+    Ok(())
 }
