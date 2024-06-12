7f71e82036be252b9bf63b476ae75ee8d8a92a7d
nordluma <maxnordlund.95@hotmail.com>
2024-06-12 22:41:41 +0300

    feat: print flags and proto

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@ use std::io;
 
 use tun_tap::Iface;
 
+const HEADER_LEN: usize = 4;
+
 fn main() -> io::Result<()> {
     let nic = Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0_u8; 1504];
-    let n_bytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x?}", n_bytes, &buf[..n_bytes]);
+    loop {
+        let n_bytes = nic.recv(&mut buf[..])?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+
+        eprintln!(
+            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
+            n_bytes - HEADER_LEN,
+            flags,
+            proto,
+            &buf[HEADER_LEN..n_bytes]
+        );
+    }
 
     Ok(())
 }
