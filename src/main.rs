b47d28b06a813eb77b3749fb738696df5d467726
nordluma <maxnordlund.95@hotmail.com>
2024-06-12 22:44:42 +0300

    fix: skip ipv4 packets

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ fn main() -> io::Result<()> {
         let n_bytes = nic.recv(&mut buf[..])?;
         let flags = u16::from_be_bytes([buf[0], buf[1]]);
         let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if proto != 0x0800 {
+            // no ipv4
+            continue;
+        }
 
         eprintln!(
             "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
