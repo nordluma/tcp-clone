ce61b42c3d94cbadfc1ea17e32e3dde4dd243ec4
nordluma <maxnordlund.95@hotmail.com>
2024-06-12 23:25:48 +0300

    feat: parse packet to `Ipv4HeaderSlice`

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,29 @@ fn main() -> io::Result<()> {
             break;
         };
 
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if proto != 0x0800 {
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
             // no ipv4
             continue;
         }
 
-        eprintln!(
-            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
-            n_bytes - HEADER_LEN,
-            flags,
-            proto,
-            &buf[HEADER_LEN..n_bytes]
-        );
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[HEADER_LEN..n_bytes]) {
+            Ok(p) => {
+                let src = p.source();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+
+                eprintln!(
+                    "{:?} -> {}b of protocol: {:?} {:?}",
+                    src,
+                    dst,
+                    proto,
+                    p.payload_len()
+                );
+            }
+            Err(e) => eprintln!("ignoring weird packet {:?}", e),
+        }
     }
 
     Ok(())
