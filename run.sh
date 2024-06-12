d193c95c35c8bb8567240c3574a80106c7777f70
nordluma <maxnordlund.95@hotmail.com>
2024-06-12 22:37:44 +0300

    fix(run.sh): terminate process after ctrl+c

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cargo b --release
 sudo setcap cap_net_admin=eip $HOME/dev/rust/tcp-clone/target/release/tcp-clone
 $HOME/dev/rust/tcp-clone/target/release/tcp-clone &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap 'kill $pid' INT TERM
 wait $pid
